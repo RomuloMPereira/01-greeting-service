3dbad48ff96a0c64d6122005b0f12fa760bdee7d
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-11-25 16:46:35 -0300

    Retrieving the configuration values

--- src/main/java/com/romulopereira/controllers/GreetingController.java
+++ src/main/java/com/romulopereira/controllers/GreetingController.java
@@ -2,23 +2,33 @@ package com.romulopereira.controllers;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.romulopereira.configuration.GreetingConfiguration;
 import com.romulopereira.models.Greeting;
 
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
+	private static final String template = "%s, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	
+	@Autowired
+	private GreetingConfiguration configuration;
+	
 	@RequestMapping("/greeting")
-	public Greeting greeting(@RequestParam(value="name", defaultValue = "World") String name) {
+	public Greeting greeting(@RequestParam(value="name", defaultValue = "") String name) {
+		
+		if(name.isEmpty()) {
+			name = configuration.getDefaultValue();
+		}
+		
 		return new Greeting(
 					counter.incrementAndGet(),
-					String.format(template, name)
+					String.format(template, configuration.getGreeting(), name)
 				);
 	}
 }
